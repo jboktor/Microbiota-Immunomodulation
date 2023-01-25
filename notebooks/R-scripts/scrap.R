8e7f396034db2261fd69cc7e50e3a7015a71f0d0
jboktor <jboktor@caltech.edu>
2023-01-24 18:16:19 -0800

    reticulate setup

--- notebooks/R-scripts/scrap.R
+++ notebooks/R-scripts/scrap.R
@@ -1,2 +1,7 @@
 
 # First R script
+library(reticulate)
+# renv::use_python(python= '/home/jboktor/miniconda3/envs/pdmbsR' )
+
+reticulate::use_condaenv(condaenv = 'pdmbsR', required = TRUE)
+reticulate::conda_install("pdmbsR", "gget", channel = "bioconda")
